b61e5a2a0d4bd6b0afa4968d5ebdcb39947088d2
Rahmal Conda <rahmal@gmail.com>
2012-07-24 18:17:27 -0700

    FuelMeGreen: Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FuelMe::Application.config.secret_token = '9f926055b851bbf0253b30c0ce03f4ace5b02cbede03a6172e5eed5a4bd91793b191282a4146751615304b00d2b73345691a5c3db6e5067167c566933aba25bc'
