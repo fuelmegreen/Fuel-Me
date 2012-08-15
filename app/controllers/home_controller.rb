191b49840ddf04f1b9a4fd87db41264c20b30fb6
Anthony <mranthonybailey@gmail.com>
2012-08-15 12:38:39 -0500

    Added Home Controller and Initial Views

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def privacy
+  end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def how_it_works
+  end
+
+  def potential_investors
+  end
+end
