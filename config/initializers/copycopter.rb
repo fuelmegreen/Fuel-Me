206da3afe00de8aec0be696137d1412dda866c27
Anthony <mranthonybailey@gmail.com>
2012-08-23 16:45:15 -0500

    Revert "Revert "copycopter changes""
    This reverts commit 4ceeec2b9e65b12a44c93857de13890456f9d3c3.

---
+++ config/initializers/copycopter.rb
@@ -0,0 +1,6 @@
+CopycopterClient.configure do |config|
+  config.api_key = '3478991a80fc4df8fcd162b569241a0c0a45153c7ad62e2f'
+  config.host = 'cms.fuelme.com'
+end
+
+<%= t('.hello', :default => 'Hello world') %>
